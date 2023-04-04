60e0af7764783e40cbae535e05d7f4c7de638020
Gustavo Michels <gustavomichels@icloud.com>
2023-04-04 15:33:15 -0400

    Add IP address validation

--- adguard/dns_config_resource.go
+++ adguard/dns_config_resource.go
@@ -2,6 +2,7 @@ package adguard
 
 import (
 	"context"
+	"regexp"
 	"time"
 
 	"github.com/gmichels/adguard-client-go"
@@ -140,6 +141,10 @@ func (r *dnsConfigResource) Schema(_ context.Context, _ resource.SchemaRequest, 
 				Default:     stringdefault.StaticString(""),
 				Validators: []validator.String{
 					stringvalidator.All(
+						stringvalidator.RegexMatches(
+							regexp.MustCompile(`\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b`),
+							"must be a valid IPv4 address",
+						),
 						checkBlockingMode("custom_ip"),
 						stringvalidator.AlsoRequires(path.Expressions{
 							path.MatchRoot("blocking_mode"),
@@ -155,6 +160,10 @@ func (r *dnsConfigResource) Schema(_ context.Context, _ resource.SchemaRequest, 
 				Default:     stringdefault.StaticString(""),
 				Validators: []validator.String{
 					stringvalidator.All(
+						stringvalidator.RegexMatches(
+							regexp.MustCompile(`(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`),
+							"must be a valid IPv6 address",
+						),
 						checkBlockingMode("custom_ip"),
 						stringvalidator.AlsoRequires(path.Expressions{
 							path.MatchRoot("blocking_mode"),
