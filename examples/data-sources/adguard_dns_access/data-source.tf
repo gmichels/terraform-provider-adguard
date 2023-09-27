e7be6f5f0b8f80081d1d8d09ff14132da44f1c9f
Gustavo Michels <gustavomichels@icloud.com>
2023-09-27 10:40:09 -0500

    refactor: remove `adguard_dns_access` and `adguard_dns_config`

--- examples/data-sources/adguard_dns_access/data-source.tf
+++
@@ -1,4 +0,0 @@
-### this data source has been DEPRECATED and will be removed in a future release
-### Use the `dns` block in the `adguard_config` data source instead
-# get the DNS access list
-data "adguard_dns_access" "test" {}
--- examples/resources/adguard_dns_access/import.sh
+++
@@ -1,3 +0,0 @@
-# DNS access list can be imported by specifying the ID as `1`
-# NOTE: there can only be 1 (one) `adguard_dns_access` resource, hence the hardcoded ID
-terraform import adguard_dns_access.test "1"
