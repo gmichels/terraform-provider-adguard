080bea31c7a56816473b4d92118af846a10bbfeb
durandguru <79765683+durandguru@users.noreply.github.com>
2023-12-30 20:01:58 +0100

    Update import.sh
    terraform import adguard_dns_config.test "1" should be terraform import adguard_config.test "1"

--- examples/resources/adguard_config/import.sh
+++ examples/resources/adguard_config/import.sh
@@ -1,3 +1,3 @@
 # DNS config can be imported by specifying the ID as `1`
 # NOTE: there can only be 1 (one) `adguard_dns_config` resource, hence the hardcoded ID
-terraform import adguard_dns_config.test "1"
+terraform import adguard_config.test "1"
